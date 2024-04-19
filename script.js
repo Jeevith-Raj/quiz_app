c44becc95adf856da19fe8a98e82eb5216ec2d83
Jeevith-Raj <20uad028@kamarajengg.edu.in>
2024-04-18 22:45:34 -0400

    this is also a commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const questions = [
+    {
+        question:"Which is the largest animmal",
+        answers:[
+            {text:"shark",correct:false},
+            {text:"Blue Whale",correct:true},
+            {text:"Elephant",correct:false},
+            {text:"Giraffe",correct:false}
+
+        ]
+    },
+    {
+        question:"Which is the largest bird",
+        answers:[
+            {text:"crow",correct:false},
+            {text:"ostrich",correct:true},
+            {text:"flamingo",correct:false},
+            {text:"hen",correct:false}
+
+        ]
+    },
+    {
+        question:"Which is the largest country",
+        answers:[
+            {text:"india",correct:false},
+            {text:"Russia",correct:true},
+            {text:"africa",correct:false},
+            {text:"china",correct:false}
+
+        ]
+    }
+];
+const questionElement = document.getElementById("q");
+const answer = document.getElementById("ans");
+const next = document.getElementById("nextbtnlet ");
+
+let currentquestionindex =0;
+let score =0;
+function startquiz(){
+    currentquestionindex = 0;
+    score = 0;
+    next.innerHTML = "Next";
+    showquestion();
+}
+
